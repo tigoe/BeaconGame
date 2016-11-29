47a30084de72ca74183bc3bfe18e9311c47f7504
tigoe <tom.igoe@gmail.com>
2016-11-29 11:21:15 -0500

    added Beacon examples

--- Beacon/CurieBleBeacon/CurieBleBeacon.ino
+++ Beacon/CurieBleBeacon/CurieBleBeacon.ino
@@ -1,9 +1,48 @@
-void setup() {
-  // put your setup code here, to run once:
 
+// Import libraries:
+#include <SPI.h>
+#include <CurieBLE.h>
+
+const int connectedLED = LED_BUILTIN;
+
+// create peripheral instance:
+BLEPeripheral peripheral = BLEPeripheral();
+// create service:
+BLEService counterService = BLEService("A495FF20-C5B1-4B44-B512-1370F02D74DE");
+// create characteristic:
+BLEIntCharacteristic counterCharacteristic = BLEIntCharacteristic("A495FF25-C5B1-4B44-B512-1370F02D74DE", BLERead | BLENotify);
+
+void setup() {
+  Serial.begin(9600);           // initialize serial
+  pinMode(connectedLED, OUTPUT);// set LED as output
+
+  // set advertised local name and service UUID:
+  peripheral.setLocalName("Beacon1");
+  peripheral.setAdvertisedServiceUuid(counterService.uuid());
+
+  // add service and characteristic:
+  peripheral.addAttribute(counterService);
+  peripheral.addAttribute(counterCharacteristic);
+
+  // start peripheral:
+  peripheral.begin();
+  counterCharacteristic.setValue(12);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  BLECentral central = peripheral.central();
+  // Only take action if there's a central connected:
+  if (central) {
+    // central connected to peripheral
+    Serial.print("Connected to central: ");
+    Serial.println(central.address());
 
+    while (central.connected()) {
+      digitalWrite(connectedLED, HIGH);
+    }
+    // when the central disconnects, turn the LED off:
+    digitalWrite(connectedLED, LOW);
+    Serial.print("Disconnected from central: ");
+    Serial.println(central.address());
+  }
 }
