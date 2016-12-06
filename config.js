3d37b6b75063866eddd15ddd33245280c6e6cbc3
tigoe <tom.igoe@gmail.com>
2016-12-06 09:25:23 -0500

    config.js update

--- config.js
+++ config.js
@@ -1,22 +1,32 @@
 // the list of users for the game:
 exports.users = [
-  {'username': 'aaa111'},   // netID
-  {'username': 'bbb222'}   // netID
+  {'username': 'aa123'},
   ];
-
-// the list of beacons for the game:
+  // the list of beacons for the game:
 exports.beacons = [
-  {'name':'beaconName','points':0, rssiLimit: -0, 'owner':null},
+  {'name':'beacon1','points':2, rssiLimit: -50, 'owner':null},
+  {'name':'beacon2','points': 20, rssiLimit: -50, 'owner':null},
+  {'name':'beacon3','points':12, rssiLimit: -50, 'owner':null},
+  {'name':'beacon4','points':6, rssiLimit: -50, 'owner':null},
+  {'name':'beacon5','points':18, rssiLimit: -50, 'owner':null},
 ];
 
-// Service UUID and beacon UUID for the game's beacons:
-exports.serviceUuid = 'AAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE';
-exports.characteristicUuid = 'AAAAAAA-BBBF-CCCC-DDDD-EEEEEEEEEEEE';
+// The RSSI value that the user submits can differ from the beacon's rssiLimit
+// by this much:
+exports.beaconLimit = 2;
 
-// details for the golden egg:
+exports.serviceUuid = 'AAAAAAAA-BBBB-CCCC-1111-1234AAAACCCC';
+exports.characteristicUuid = 'AAAAAA20-BBBB-CCCC-1111-1234AAAACCCC';
+exports.adminKey = 'secret!';
+
 exports.goldenEgg = {
-  serviceName: 'name',
-  uuid: 'AAAAAAA-BBBB-CCCC-DDDD-EEEEEEEEEEEE',
+  serviceName: 'goldenEgg',
+  name: 'goldenEgg',
+  serviceUuid: 'AAAAAAAA-BBBB-CCCC-1111-1234AAAACCCC',
+  characteristicUuid: 'AAAAAA20-BBBB-CCCC-1111-1234AAAACCCC',
+  points: 1,
+  rssi: --50,
+  owner: null,
   host: '1.1.1.1',
   port: 8080
 };
